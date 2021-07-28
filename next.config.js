92e36b547515df031c95ec2acd92e02f9f1f0e51
David Whittemore <github@xpollen8.com>
2021-07-27 22:52:27 -0700

    dw: initial checkin

---
+++ next.config.js
@@ -0,0 +1,31 @@
+const CopyPlugin = require("copy-webpack-plugin")
+
+module.exports = {
+	publicRuntimeConfig: {
+		// Will be available on both server and client
+		staticFolder: '/htdocs',
+	},
+	serverRuntimeConfig: {
+		PROJECT_ROOT: __dirname,
+	},
+	target: "server",
+	future: {
+			webpack5: true,
+	},
+	webpack: function (config, { dev, isServer }) {
+			// Fixes npm packages that depend on `fs` module
+			if (!isServer) {
+					config.resolve.fallback.fs = false
+			}
+			// copy files you're interested in
+			if (!dev) {
+					config.plugins.push(
+							new CopyPlugin({
+									patterns: [{ from: "htdb/**", to: "." }],
+							})
+					)
+			}
+
+			return config
+	},
+}
---
+++ pages/[[...path]].js
@@ -0,0 +1,37 @@
+import { join } from "path";
+import getConfig from 'next/config'
+import Head from 'next/head'
+import HTDB from 'HTDBjs';
+
+const { serverRuntimeConfig } = getConfig()
+
+let htdb;
+
+export async function getServerSideProps({ params: { path = [] } = {} }) {
+	if (!htdb) {
+		if (process.env.NODE_ENV === "production") {
+			htdb = new HTDB(join(process.cwd(), ".next/server/chunks"), 0);
+		} else {
+			const { serverRuntimeConfig } = getConfig();
+			htdb = new HTDB(serverRuntimeConfig.PROJECT_ROOT, 1);
+		}
+	}
+
+	return {
+		props: { body: await htdb.render(path.join('/')) }
+	}
+}
+
+const App = ({ body }) => (
+	<div className="container">
+		<Head>
+			<title>Sumosonic</title>
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+		<>
+			{ <div dangerouslySetInnerHTML={{ __html: body }} /> }
+		</>
+	</div>
+)
+
+export default App;
